e2df64cb82c1af8f9c9f1bc43d5c811b9fd7cfb1
Monkey Liang <i@aq741.com>
2016-10-22 16:51:00 +0800

    add login UI

---
+++ loginwindow.cpp
@@ -0,0 +1,26 @@
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+
+#include <QMessageBox>
+
+LoginWindow::LoginWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginWindow)
+{
+    ui->setupUi(this);
+}
+
+LoginWindow::~LoginWindow()
+{
+    delete ui;
+}
+
+void LoginWindow::on_loginButton_clicked()
+{
+    QString username = ui->editUser->text(),
+            password = ui->editPass->text(),
+            server = ui->editServer->text();
+    if (username.isEmpty() || password.isEmpty() || server.isEmpty())
+        return;
+
+}
